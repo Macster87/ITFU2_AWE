23ba49db515c0101a58a5ec3ae9889606d22dc98
Mapster <macster87@gmail.com>
2018-12-10 12:49:09 +0100

    Mehrdimensionale Arrays so angepasst, dass die Anzeige auch bei hohen Zahlen gut aussieht.

--- src/Mehrdimensional.java
+++ src/Mehrdimensional.java
@@ -1,5 +1,3 @@
-import java.lang.Math;
-
 public class Mehrdimensional {
 
     private static char [][] t = {  { 'a', 'b', 'c', 'd'  },
@@ -33,6 +31,10 @@ public class Mehrdimensional {
         }
     }
 
+    /* ##########################
+    AUFGABEN: Magisches Quadrat
+    ########################## */
+
     public static void magischesQuadrat(int n) {
 
         // Erstes wichtiges Kriterium
@@ -41,57 +43,174 @@ public class Mehrdimensional {
         } else {
             int[][] quadrat = new int[n][n];
 
+            // Die 1 setzen
             int x = 0;
-            int y = n/2;
-
+            int y = n/2; // ungrader int durch 2 geteilt wird automatisch abgerundet
             int k = 1;
-
             quadrat[x][y] = k;
 
+            // x = Reihe // y = Spalte
+
             while(k < n*n) {
+                // Ersteinmal um 1 erhöhen
                 k++;
-                // Fall c) : Muss zuerst wegen Check auf bereits belegte Zelle.
-                if ((x - 1 < 0 && y + 1 >= n) || (quadrat[x-1][y+1] != 0)) {
-                    y = y + 1;
-                    quadrat[x][y] = k;
-                // Fall a)
-                } else if(x-1 < 0 && y+1 < n) {
+
+                // Regel a)
+                // Wenn x nach oben raus geht, aber y noch im Rahmen liegt
+                if(x-1 < 0 && y+1 <= n-1) {
                     x = n-1;
                     y = y+1;
-                    quadrat[x][y] = k;
-                // Fall b)
-                } else if(x-1 < n && y+1 >= n) {
+                //Regel b)
+                // Wenn x im Rahmen liegt, aber y nach rechts raus geht
+                } else if(x-1 >= 0 && y+1 > n-1) {
                     x = x-1;
                     y = 0;
-                    quadrat[x][y] = k;
-                // Normalfall
+                // Regel c)
+                // Wenn x nach oben raus geht und y nach rechts raus geht.
+                } else if(x-1 < 0 && y+1 > n-1) {
+                    x = x+1;
+                } else if(quadrat[x-1][y+1] != 0) {
+                    x = x+1;
                 } else {
-                    x = x - 1;
-                    y = y + 1;
-                    quadrat[x][y] = k;
+                    x = x-1;
+                    y = y+1;
                 }
+
+                quadrat[x][y] = k;
             }
 
-
-            //System.out.println(quadrat[0][n/2]);
             showQuadrat(quadrat);
+            checkQuadrat(quadrat);
         }
     }
 
-    // Hilfsmethode zur Ausgabe des Quadrats in der Konsole
+    // Hilfsmethode zur Ausgabe des Quadrats in der Konsole, damit es schön aussieht
     private static void showQuadrat(int[][] quadrat) {
+
+        int maximaleZahlengroesse = getDigits(quadrat[0].length * quadrat[0].length);
+
         for(int i = 0; i < quadrat[0].length; i++) {
             for(int j = 0; j < quadrat[0].length; j++) {
-                System.out.print(quadrat[i][j] + " ");
+                // Leerzeichen vor die Zahlen wenn sie entsprechend klein sind
+                String result = "";
+                for(int m = 0; m < maximaleZahlengroesse-getDigits(quadrat[i][j]); m++) {
+                    result = result + " ";
+                }
+
+                System.out.print(result + quadrat[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    // Hilfsmethode: Anzahl an Stellen in einer Integer-Zahl
+    private static int getDigits(int number) {
+        if (number < 100000) {
+            if (number < 100) {
+                if (number < 10) {
+                    return 1;
+                } else {
+                    return 2;
+                }
+            } else {
+                if (number < 1000) {
+                    return 3;
+                } else {
+                    if (number < 10000) {
+                        return 4;
+                    } else {
+                        return 5;
+                    }
+                }
+            }
+        } else {
+            if (number < 10000000) {
+                if (number < 1000000) {
+                    return 6;
+                } else {
+                    return 7;
+                }
+            } else {
+                if (number < 100000000) {
+                    return 8;
+                } else {
+                    if (number < 1000000000) {
+                        return 9;
+                    } else {
+                        return 10;
+                    }
+                }
+            }
+        }
+    }
+
+    // Prüfen ob das Quadrat richtig ist, mit der Quersumme jeder Zeile, Spalte und den beiden Diagonalen
+    private static void checkQuadrat(int[][] quadrat) {
+
+        boolean zeilenCheck = true;
+        boolean spaltenCheck = true;
+        boolean diagonalenCheck = true;
+
+        int checksumme = 0;
+        int temp = 0;
+
+        // Zeilen checken
+        for(int i = 0; i < quadrat[0].length; i++) {
+
+            for(int j = 0; j < quadrat[0].length; j++) {
+                temp = temp + quadrat[i][j];
+            }
+            if(checksumme == 0) {
+                checksumme = temp;
+            } else if (temp != checksumme){
+                zeilenCheck = false;
+            }
+            temp = 0;
+        }
+
+        // Spalten checken
+        for(int i = 0; i < quadrat[0].length; i++) {
+
+            for(int j = 0; j < quadrat[0].length; j++) {
+                temp = temp + quadrat[j][i];
+            }
+            if(checksumme == 0) {
+                checksumme = temp;
+            } else if (temp != checksumme){
+                spaltenCheck = false;
+            }
+            temp = 0;
+        }
+
+        // 1. Diagonalen checken
+        for(int i = 0; i < quadrat[0].length; i++) {
+            temp = temp + quadrat[i][i];
+        }
+        if(temp != checksumme) {
+            diagonalenCheck = false;
+        }
+        temp = 0;
+
+        // 2. Diagonale checken
+        for(int i = 0; i < quadrat[0].length; i++) {
+            temp = temp + quadrat[quadrat[0].length-1][i];
+        }
+        if(temp != checksumme) {
+            diagonalenCheck = false;
+        }
+
+        System.out.println();
+        System.out.println("Checksumme = " + checksumme);
+        System.out.println("Zeilencheck = " + zeilenCheck);
+        System.out.println("Spaltencheck = " + spaltenCheck);
+        System.out.println("Diagonalencheck = " + diagonalenCheck);
+
     }
 
     public static void main(String[] args) {
         //EinsA();
         //EinsB();
         //EinsC();
-        magischesQuadrat(7);
+        magischesQuadrat(9);
     }
 }
