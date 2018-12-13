84ce7fd18a128d769ba04a65c14a92deca01f571
Mapster <macster87@gmail.com>
2018-12-13 15:20:17 +0100

    Verbesserung

--- src/Pascalschedreieck.java
+++ src/Pascalschedreieck.java
@@ -1,12 +1,12 @@
 public class Pascalschedreieck {
 
     private static void pascal(int n) {
-        int[][] mainarray = new int[n][];
 
-        for(int i = 1; i < n; i++) {
-            int[] subarray = new int[i];
-            subarray[i] = 1;
-            mainarray[i] = subarray;
+        int[][] mainarray = new int[n][];
+        int pascalStart = 1;
+
+        for(int i = 0; i < n; i++) {
+            mainarray[i] = new int[i+1];
         }
 
         showPascal(mainarray);
@@ -15,13 +15,13 @@ public class Pascalschedreieck {
     private static void showPascal(int[][] d) {
         for(int i = 0; i < d.length; i++) {
             for(int j = 0; j < d[i].length; j++) {
-                System.out.print(String.format("%3d ",d[i][j]) + " ");
+                System.out.print(d[i][j] + " ");
             }
             System.out.println(); // Neue Zeile
         }
     }
 
     public static void main(String[] args) {
-        pascal(3);
+        pascal(5);
     }
 }
