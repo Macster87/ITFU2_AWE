b14aacb732688734b5819d0c04bf5e13a1c166ca
Mapster <macster87@gmail.com>
2018-12-14 13:22:01 +0100

    Lösung funktioniert nun.

--- src/Pascalschedreieck.java
+++ src/Pascalschedreieck.java
@@ -2,22 +2,56 @@ public class Pascalschedreieck {
 
     private static void pascal(int n) {
 
-        int[][] mainarray = new int[n][];
-        int pascalStart = 1;
+        if (n > 29) {
+            System.out.println("Integer-Begrenzung wird überschritten");
+        } else {
+            int[][] mainarray = new int[n+1][];
 
-        for(int i = 0; i < n; i++) {
-            mainarray[i] = new int[i+1];
+            for (int reihe = 0; reihe <= n; reihe++) {
+                mainarray[reihe] = new int[reihe+1];
+                int feld = 1;
+
+                for (int zeile = 0; zeile <= reihe; zeile++) {
+                    if(zeile == 0) {
+                        feld = 1;
+                    } else {
+                        feld = feld * ((reihe + 1) - zeile) / zeile;
+                    }
+                    mainarray[reihe][zeile] = feld;
+                }
+            }
+            showPascal(mainarray);
         }
-
-        showPascal(mainarray);
     }
 
     private static void showPascal(int[][] d) {
-        for(int i = 0; i < d.length; i++) {
-            for(int j = 0; j < d[i].length; j++) {
-                System.out.print(d[i][j] + " ");
+
+        // Anzahl der Stellen der größten Zahl bestimmen.
+        // Die ist immer ganz unten in der Mitte.
+        // Anhand dessen wird die Anzahl der vorangestellten Leerzeichen bestimmt und der
+        // Abstand für z.B. printf(%6d) weiter unten. Bei max. 3-stelligen Zahlen reicht auch %4d
+        String buffer = "";
+        int buffercount = String.valueOf(d[d.length-1][d[d.length-1].length/2]).length();
+        if(buffercount%2 != 0) {
+            buffercount = (buffercount+1)/2;
+        }
+
+        for(int i = 1; i <= buffercount; i++) {
+            buffer = buffer + " ";
+        }
+
+        // Baum zeichnen
+        for (int zeile = 0; zeile < d.length; zeile++) {
+
+            //Vorangestellte Leerzeichen einfügen
+            for (int j = d.length - zeile; j > 1; j--) {
+                System.out.print(buffer);
             }
-            System.out.println(); // Neue Zeile
+
+            for (int spalte = 0; spalte <= zeile; spalte++) {
+                    System.out.printf("%"+buffercount*2+"d", d[zeile][spalte]);
+            }
+            System.out.println();
         }
     }
 
