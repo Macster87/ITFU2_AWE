bf3bdc6333033544c78ab95a6a6d47a16d080d27
Mapster <macster87@gmail.com>
2019-01-09 13:46:13 +0100

    Weitere Kommentare hinzugefügt.

--- src/Methode.java
+++ src/Methode.java
@@ -1,10 +1,25 @@
 public class Methode {
+
+    // Vorgegebene Arrays
     private static int[] a1 = {10, 20, 1, 8, 9};
     private static int[] a2 = {1, 2, 17, 3, 8};
     private static int[] a3 = {5, 6, 22, 80, 2};
     private static int[] a4 = {7, 8, 42, 2, 2, 4711};
     private static int[] a5 = {80, 8086, 6502, 68000, -753};
 
+
+    /**
+     * Eine Methode um in einem gegebenen Array die Position(!) der kleinsten Zahl
+     * zurückzugeben. Wenn in einem Array 2 oder mehr Zahlen die Kleinste sind (siehe a4),
+     * dann soll die Erste, die weiter links steht als Position zurückgegeben werden.
+     *
+     * Dazu gehen wir das Array mit einer ForEach-Schleife durch und merken uns die Aktuelle Position, sowie
+     * die bisher kleinste. Das Problem mit den 2 gleichen kleinsten Zahlen umgehen wir, indem wir im if-Block ein
+     * Kleiner (<) statt einem Kleiner-Gleich (<=) verwenden.
+     *
+     * @param input Ein beliebiges Array mit Integer-Zahlen
+     * @return Die Positions-Nummer im Array der kleinsten Zahl.
+     */
     private static int getSmallestNumber(int[] input) {
 
         int smallestPosition = 0;
