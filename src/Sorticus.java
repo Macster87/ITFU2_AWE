db2ca0cc874276d6052aa00b66cb07df35b40989
Mapster <macster87@gmail.com>
2019-01-10 15:06:19 +0100

    - Bubble-Sort hinzugefügt - Dokumentation hinzugefügt

---
+++ src/Sorticus.java
@@ -0,0 +1,89 @@
+public class Sorticus {
+
+    // Vorgegebene Arrays
+    private static int[] a1 = {10, 20, 1, 8, 9};
+    private static int[] a2 = {1, 2, 17, 3, 8};
+    private static int[] a3 = {5, 6, 22, 80, 2};
+    private static int[] a4 = {7, 8, 42, 2, 2, 4711};
+    private static int[] a5 = {80, 8086, 6502, 68000, -753};
+
+    /**
+     * Methode zur Bestimmung der Position der kleinsten Zahl in einem Array. Ähnlich wie die Methode in der Klasse "Methode"
+     * jedoch mit einem zusätzlichem Start-Parameter, wenn man die Suche von anderer Stelle aus vorwärts ausführen möchte.
+     * @param input Ein beliebiges Array mit Integer-Zahlen
+     * @param start Startposition. Die Suche wird rechts dieser Position ablaufen.
+     * @return Die Positions-Nummer im Array der kleinsten Zahl im gewähltem Abschnitt.
+     */
+    private static int getSmallestNumberPosition(int[] input, int start) {
+        int min = 2147483647;
+
+        for(int i = start; i < input.length; i++) {
+            if(input[i] < min) {
+                min = input[i];
+                start = i;
+            }
+        }
+        return start;
+    }
+
+    /**
+     * Standard Selection-Sort Algorithmus
+     * @param input Ein beliebiges Array mit Integer-Zahlen
+     * @return Jenes Array aufsteigend sortiert
+     */
+    private static int[] selectionSort(int[] input) {
+
+        int tmp;
+
+        for(int i = 0; i < input.length-1; i++) {
+            int position = getSmallestNumberPosition(input, i+1);
+                if(input[position] < input[i]) {
+                    tmp = input[position];
+                    input[position] =  input[i];
+                    input[i] = tmp;
+                }
+        }
+
+        return input;
+    }
+
+    /**
+     * Standard Bubble-Sort Algorithmus
+     * @param input Ein beliebiges Array mit Integer-Zahlen
+     * @return Jenes Array aufsteigend sortiert
+     */
+    private static int[] bubbleSort(int[] input) {
+        boolean finished = false;
+
+        int tmp;
+        int changes = 1;
+
+        while(changes != 0) {
+            changes = 0;
+            for(int i = 0; i < input.length-1; i++) {
+                if(input[i] > input[i+1]) {
+                    tmp = input[i];
+                    input[i] =  input[i+1];
+                    input[i+1] = tmp;
+                    changes++;
+                }
+            }
+        }
+        return input;
+    }
+
+    /**
+     * Hilfsfunktion zur Ausgabe eines Arrays in der Konsole
+     * @param input Ein beliebiges Array mit Integer-Zahlen
+     */
+    private static void showArray(int[] input) {
+        for(int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        showArray(bubbleSort(a5));
+    }
+
+}
